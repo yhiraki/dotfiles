1434816a50accbbdbc6705ab892a80622927c3b7
yhiraki <coffexpr@gmail.com>
2016-05-29 23:13:35 +0900

    .gitを削除

--- install.d/pyenv.sh
+++ install.d/pyenv.sh
@@ -2,5 +2,5 @@
 
 export PYENV_ROOT=$HOME/.pyenv
 
-ln -s $GOPATH/src/github.com/yyuu/pyenv.git $PYENV_ROOT
-ln -s $GOPATH/src/github.com/yyuu/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
+ln -s $GOPATH/src/github.com/yyuu/pyenv $PYENV_ROOT
+ln -s $GOPATH/src/github.com/yyuu/pyenv-virtualenv $PYENV_ROOT/plugins/pyenv-virtualenv
