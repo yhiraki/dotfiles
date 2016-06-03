96dd050893bcf1ecfecd6e389d5a2e46d093a917
yhiraki <coffexpr@gmail.com>
2016-06-03 13:00:26 +0900

    追加

--- install.d/ghq.sh
+++ install.d/ghq.sh
@@ -2,9 +2,11 @@
 
 GHQ_INSTALL_LIST=$(echo '
 junegunn/fzf
+lysyi3m/osx-terminal-themes
 motemen/ghq
 yyuu/pyenv
 yyuu/pyenv-virtualenv
+github.com/vimpr/vimperator-colors
 ' | xargs)
 
 for p in $GHQ_INSTALL_LIST; do
