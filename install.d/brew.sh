07e09f931b28e36535c17d4c953e6f035fea4639
awa-manju <awa-manju@gmail.com>
2016-06-03 12:02:28 +0900

    追加

--- install.d/brew.sh
+++ install.d/brew.sh
@@ -4,6 +4,28 @@ BREW_INSTALL_LIST=$(echo '
 git
 zsh
 golang
+autoconf
+ctags
+gdbm
+gmp
+gnutls
+go
+libevent
+libtasn1
+nettle
+openssl
+pcre
+pkg-config
+postgresql
+pyenv
+pyenv-virtualenv
+readline
+reattach-to-user-namespace
+task
+tmux
+tree
+vim
+wget
 ' | xargs)
 
 brew install $BREW_INSTALL_LIST
