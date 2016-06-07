f5dc67d93e4dc6a9f9dfdc8c971e7846e4b2e91c
awa-manju <awa.manju@gmail.com>
2016-06-07 09:24:52 +0900

    mod

--- install.d/pyenv.sh
+++ install.d/pyenv.sh
@@ -7,4 +7,4 @@ mkdir $PYENV_ROOT/plugins
 ln -s $GOPATH/src/github.com/yyuu/pyenv $PYENV_ROOT
 ln -s $GOPATH/src/github.com/yyuu/pyenv-virtualenv $PYENV_ROOT/plugins/pyenv-virtualenv
 ln -s $GOPATH/src/github.com/jawshooah/pyenv-default-packages $PYENV_ROOT/plugins/pyenv-default-packages
-ln -s $ZDOTPATH/pyenv/default-packages $PYENV_ROOT/
+ln -s $ZDOTDIR/pyenv/default-packages $PYENV_ROOT/
