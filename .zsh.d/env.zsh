eb631c3223f129f03e6be2d89ee82e59c5a46a91
awa-manju <awa.manju@gmail.com>
2016-06-10 00:58:21 +0000

    envファイルを分割

---
+++ .zsh.d/env.zsh
@@ -0,0 +1,16 @@
+export ZDOTDIR=$HOME/src/github.com/awa-manju/dotfiles
+
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+
+export LANG=ja_JP.UTF-8
+
+export EDITOR=nvim
+# export EDITOR=vim
+
+export HISTFILE=${HOME}/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=100000
+
+export GOPATH=$HOME
+export PATH=$PATH:$GOPATH/bin
