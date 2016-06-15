a17e737a6ed45f77d68405c9ac3a7179e55c3b12
awa-manju <awa.manju@gmail.com>
2016-06-15 23:26:57 +0900

    mod

--- install.d/link.sh
+++ install.d/link.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-ORIG_DOTFILES=$(echo '
-.zshenv
-.tmux.conf
-.vim
-.git_template
-.vimperatorrc
-' | xargs)
-
-for f in $ORIG_DOTFILES; do
-  if [ -e $HOME/$f ]; then
-    mv $HOME/$f /tmp/${f}.orig
-  fi
-  ln -s $ZDOTDIR/$f $HOME/$f
-done
+ln -s $ZDOTDIR/.zshenv $HOME
+ln -s $DOTDIR/.tmux.conf $HOME
+ln -s $DOTDIR/.vim $HOME
+ln -s $DOTDIR/.git_template $HOME
+ln -s $DOTDIR/.vimperatorrc $HOME
 
 # tmux plugins
 TMUXDIR=$HOME/.tmux
@@ -22,4 +13,4 @@ ln -s $HOME/src/github.com/tmux-plugins/tpm $TMUXDIR/plugins/tpm
 
 # neovim
 mkdir -p $XDG_CONFIG_HOME/nvim
-ln -s $ZDOTDIR/.vim/vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s $DOTDIR/.vim/vimrc $XDG_CONFIG_HOME/nvim/init.vim
--- install.d/pyenv.sh
+++ install.d/pyenv.sh
@@ -2,12 +2,12 @@
 
 export PYENV_ROOT=$HOME/.pyenv
 
-mkdir -p $PYENV_ROOT/plugins
-
 ln -s $GOPATH/src/github.com/yyuu/pyenv $PYENV_ROOT
+
+mkdir $PYENV_ROOT/plugins
 ln -s $GOPATH/src/github.com/yyuu/pyenv-virtualenv $PYENV_ROOT/plugins/pyenv-virtualenv
 ln -s $GOPATH/src/github.com/jawshooah/pyenv-default-packages $PYENV_ROOT/plugins/pyenv-default-packages
-ln -s $ZDOTDIR/pyenv/default-packages $PYENV_ROOT/
+ln -s $DOTDIR/pyenv/default-packages $PYENV_ROOT/
 
 source $ZDOTDIR/.zprofile
 
