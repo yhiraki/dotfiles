342c1dc597a388cbb7a1a28e0e94321050928854
yhiraki <coffexpr@gmail.com>
2016-08-05 12:08:19 +0900

    設定ファイルの管理をzplugに統一

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -10,6 +10,23 @@ zplug "zsh-users/zsh-completions"
 zplug "felixr/docker-zsh-completion"
 zplug "mafredri/zsh-async"
 zplug "sindresorhus/pure"
+zplug "aws/aws-cli", use:bin/aws_zsh_completer.sh, on:"zsh-users/zsh-completions", nice:10
+
+zplug "$ZDOTDIR/func.zsh", from:local
+zplug "$ZDOTDIR/anyframe.zsh", from:local, on:"mollifier/anyframe"
+zplug "$ZDOTDIR/alias.zsh", from:local
+
+case ${OSTYPE} in
+  darwin*)
+    zplug "$ZDOTDIR/darwin.zsh", from:local
+    ;;
+  linux*)
+    zplug "$ZDOTDIR/linux.zsh", from:local
+    ;;
+  msys*)
+    zplug "$ZDOTDIR/msys.zsh", from:local
+    ;;
+esac
 
 # check installed
 if ! zplug check --verbose; then
@@ -19,7 +36,6 @@ if ! zplug check --verbose; then
     # fi
 fi
 
-# load plugins
 zplug load
 
 # 自動補完を有効化
