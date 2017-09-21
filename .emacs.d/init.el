9de3cd47d3a57deb7e267ea298db8ee63a5528e3
yhiraki <coffexpr@gmail.com>
2017-09-21 20:27:49 +0900

    mod

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -779,6 +779,15 @@ to next line."
           ))
 
 
+;;;;;;;;;;;
+;; emmet ;;
+;;;;;;;;;;;
+
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook 'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook 'emmet-mode)
+
 ;;;;;;;;;
 ;; sql ;;
 ;;;;;;;;;
