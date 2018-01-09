6cb8e917bd11cedabb4f252585f5f669b57b5b47
coffexpr <coffexpr@gmail.com>
2018-01-09 16:57:09 +0900

    mod

--- .emacs.d/init/yasnippet.el
+++ .emacs.d/init/yasnippet.el
@@ -5,16 +5,15 @@
                    (list
                     (locate-user-emacs-file "snippets")
                     "~/.yasnippet"
-                    'yas-installed-snippets-dir
+                    ;; 'yas-installed-snippets-dir
                     ))
              (yas-global-mode 1)
-
-             (eval-after-load 'yasnippet
-               '(progn
-                  (define-key yas-keymap (kbd "RET") 'yas-next-field-or-maybe-expand)))
-
              (setq yas-prompt-functions '(yas-popup-isearch-prompt yas-ido-prompt yas-no-prompt))
+             :bind (:map yas-keymap
+                         ("RET" . yas-next-field-or-maybe-expand))
              )
+
+(use-package yasnippet-snippets :ensure t)
 
 ;; use popup menu for yas-choose-value
 ;; https://www.emacswiki.org/emacs/Yasnippet
