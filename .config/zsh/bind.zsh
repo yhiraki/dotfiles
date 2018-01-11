e692f5f05eb4da0a9af9ce1a77962e45195c8160
yhiraki <coffexpr@gmail.com>
2018-01-11 13:00:19 +0900

    add bind for zsh

---
+++ .config/zsh/bind.zsh
@@ -0,0 +1,18 @@
+# vi bind でも emacs bind を使用する
+# https://qiita.com/b4b4r07/items/8db0257d2e6f6b19ecb9
+bindkey -M viins '\er' history-incremental-pattern-search-forward
+bindkey -M viins '^?'  backward-delete-char
+bindkey -M viins '^A'  beginning-of-line
+bindkey -M viins '^B'  backward-char
+bindkey -M viins '^D'  delete-char-or-list
+bindkey -M viins '^E'  end-of-line
+bindkey -M viins '^F'  forward-char
+bindkey -M viins '^G'  send-break
+bindkey -M viins '^H'  backward-delete-char
+bindkey -M viins '^K'  kill-line
+bindkey -M viins '^N'  down-line-or-history
+bindkey -M viins '^P'  up-line-or-history
+bindkey -M viins '^R'  history-incremental-pattern-search-backward
+bindkey -M viins '^U'  backward-kill-line
+bindkey -M viins '^W'  backward-kill-word
+bindkey -M viins '^Y'  yank
