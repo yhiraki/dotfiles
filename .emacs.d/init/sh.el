62c49521daee0cadcd45471113e20fb29e6fdcea
yhiraki <coffexpr@gmail.com>
2018-01-15 15:15:49 +0900

    add shellscript-mode indent settings

--- .emacs.d/init/sh.el
+++ .emacs.d/init/sh.el
@@ -1,3 +1,8 @@
 (use-package sh
   :mode
-  ("\\.zsh\\'" . shell-script-mode))
+  ("\\.zsh\\'" . shell-script-mode)
+  :config
+  (setq sh-basic-offset 4
+        sh-indentation 4
+        sh-indent-for-case-label 0
+        sh-indent-for-case-alt '+))
