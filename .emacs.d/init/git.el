34869800ca00f8e38f20326651710922682a3109
coffexpr <coffexpr@gmail.com>
2018-01-18 14:33:30 +0900

    git commit 時に diff を表示しないように

--- .emacs.d/init/git.el
+++ .emacs.d/init/git.el
@@ -1,4 +1,9 @@
-(use-package magit :ensure t)
+(use-package magit
+  :ensure t
+  :config
+  ;; magit-commmit 時に diff が開くのをやめる
+  ;; https://qiita.com/egg_chicken/items/948f8df70069334e8296
+  (remove-hook 'server-switch-hook 'magit-commit-diff))
 
 (use-package git-gutter-fringe+
   :ensure t
