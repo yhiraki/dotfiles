fc7ed4c43a02a68f02e19fe50003d17fbdfa793e
coffexpr <coffexpr@gmail.com>
2018-01-18 16:07:36 +0900

    add modeline settings

---
+++ .emacs.d/init/appearance.el
@@ -0,0 +1,19 @@
+(use-package symon
+  :init (el-get-bundle zk-phi/symon)
+  :config (symon-mode))
+
+;; バッテリ残量をおしゃれに表示
+;; https://qiita.com/zk_phi/items/76c950c89428a54ec67d
+(add-to-list 'mode-line-format
+             '(:eval (let ((bat (read (cdr (assoc ?p (funcall battery-status-function))))))
+                       (cond ((> bat 87)  "█") ((> bat 75)  "▇")
+                             ((> bat 62)  "▆") ((> bat 50)  "▅")
+                             ((> bat 37)  "▄") ((> bat 25)  "▃")
+                             ((> bat 12)  "▂") (t           "▁")))))
+
+(use-package sky-color-clock
+  :init (el-get-bundle zk-phi/sky-color-clock)
+  :config
+  (setq sky-color-clock-enable-emoji-icon nil
+        sky-color-clock-format "%m/%d %H:%M")
+  (sky-color-clock-initialize 35))
