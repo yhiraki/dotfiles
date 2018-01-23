84457f7b9191602c58ed7837af139ee829cb4e55
yhiraki <coffexpr@gmail.com>
2018-01-23 10:47:46 +0900

    create function file path join.

--- emacs/init/global.el
+++ emacs/init/global.el
@@ -1,2 +1,7 @@
 (use-package s :ensure t)
 (use-package f :ensure t)
+
+;; path 連結
+;; http://tototoshi.hatenablog.com/entry/20110520/1305906664
+(defun my/file-path-join (&rest paths)
+  (reduce #'(lambda (x y) (concat (file-name-as-directory x) y)) paths))
