fe5a5061fc20f4725fa269dc28f4a86ff1f4683a
yhiraki <coffexpr@gmail.com>
2018-01-24 16:57:17 +0900

    change direx mode popwin setting

--- emacs/init/popwin.el
+++ emacs/init/popwin.el
@@ -8,7 +8,7 @@
           ("*quickrun*" :align below :select nil :ratio 0.3)
           ("*magit: \*" :regexp t :align below :ratio 0.3)
           ("*magit-diff: \*" :regexp t :align above :ratio 0.5)
-          (direx:direx-mode :popup t :align left :ratio 0.2)
+          (direx:direx-mode :popup t :align left :ratio 32)
           ("*Warnings*" :popup t :align below :ratio 0.1)
           )
         shackle-lighter "")
