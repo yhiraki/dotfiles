7dac5eb8ce8c58183575c9f2f0a85ab630bfa7fa
yhiraki <coffexpr@gmail.com>
2018-02-01 13:45:28 +0900

    pyenv -> anyenv

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -2,19 +2,21 @@
 (use-package company-jedi :ensure t)
 (use-package switch-buffer-functions :ensure t)
 
+(defvar pyenv-dir "~/.anyenv/envs/pyenv/")
+
 (defun switch-jedi-server ()
   "Automatically activates pyenv version if .python-version file exists."
   (f-traverse-upwards
    (lambda (path)
      (let ((pyenv-version-path (f-expand ".python-version" path)))
        (cond ((f-exists? pyenv-version-path)
-              (setq-default my/current-virtual-env (concat "~/.pyenv/versions/" (s-trim (f-read-text pyenv-version-path 'utf-8)))
+              (setq-default my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text pyenv-version-path 'utf-8)))
                             jedi:server-args (list "--virtual-env" my/current-virtual-env))
               ))))))
 (add-hook 'switch-buffer-functions
           (lambda (prev cur) (switch-jedi-server)))
 
-(setq-default my/current-virtual-env (concat "~/.pyenv/versions/" (s-trim (f-read-text "~/.pyenv/version" 'utf-8)))
+(setq-default my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text (concat pyenv-dir "/version") 'utf-8)))
               jedi:server-args (list "--virtual-env" my/current-virtual-env))
 
 (use-package python
