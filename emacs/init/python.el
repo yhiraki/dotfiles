5b792438e6d8e8c29fbda51d54d720f5486dc0c8
coffexpr <coffexpr@gmail.com>
2018-03-06 16:55:22 +0900

    add autopep8

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -26,3 +26,12 @@
   :config
   (add-to-list 'company-backends 'company-jedi)
   (electric-indent-mode +1))
+
+(use-package py-autopep8
+  :init (el-get-bundle fujimisakari/py-autopep8.el)
+  :ensure t
+  ;; (define-key python-mode-map (kbd "C-c F") 'py-autopep8)          ; バッファ全体のコード整形
+  ;; (define-key python-mode-map (kbd "C-c f") 'py-autopep8-region)   ; 選択リジョン内のコード整形
+  ;; 保存時にバッファ全体を自動整形する
+  ;; (add-hook 'before-save-hook 'py-autopep8-before-save)
+  )
