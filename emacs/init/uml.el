0e51f45acebc283378d9e1af1a2739f894a3025e
coffexpr <coffexpr@gmail.com>
2018-03-14 12:12:58 +0900

    add flycheck-plantuml

--- emacs/init/uml.el
+++ emacs/init/uml.el
@@ -1,11 +1,15 @@
 (use-package plantuml-mode
   :ensure t
   :init
-  (setq plantuml-jar-path (expand-file-name "~/lib/java/plantuml.jar")
-        indent-tabs-mode nil
-        tab-width 2)
+  (setq plantuml-jar-path (expand-file-name "~/lib/java/plantuml.jar"))
   :mode
   ("\\.pu\\'" . plantuml-mode)
   ("\\.uml\\'" . plantuml-mode)
   ("\\.puml\\'" . plantuml-mode)
   ("\\.plantuml\\'" . plantuml-mode))
+
+(use-package flycheck-plantuml
+  :ensure t
+  :after plantuml-mode
+  :config
+  (flycheck-plantuml-setup))
