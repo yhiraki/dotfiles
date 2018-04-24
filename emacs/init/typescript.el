c3b50ef6b1098f915e1176eab17fb6ff35d3c6d1
coffexpr <coffexpr@gmail.com>
2018-04-24 09:39:28 +0900

    add emacs settings for json, typescript

--- emacs/init/typescript.el
+++ emacs/init/typescript.el
@@ -1,17 +1,18 @@
+(use-package tide :ensure t)
+
 (use-package typescript-mode
   :ensure t
+  :init
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode t)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode t)
+    (setq tide-completion-ignore-case t)
+    (company-mode-on))
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
   :mode
   ("\\.ts\\'" . typescript-mode)
-  )
-
-(use-package tide
-  :ensure t
-  :init
-  (add-hook 'typescript-mode-hook
-            (lambda ()
-              (tide-setup)
-              (flycheck-mode t)
-              (setq flycheck-check-syntax-automatically '(save mode-enabled))
-              (eldoc-mode t)
-              (company-mode-on)))
   )
