fdfa92290f098e089d5b0e826b7b9fc76cbe604e
coffexpr <coffexpr@gmail.com>
2018-05-07 15:22:04 +0900

    mod flyspell settings

--- emacs/init/flyspell.el
+++ emacs/init/flyspell.el
@@ -16,6 +16,37 @@
         (setq args "-d en_US")))
       args))
 
+  ;; ;; ispell-cmd-args is useless, it's the list of *extra* arguments we will append to the ispell process when "ispell-word" is called.
+  ;; ;; ispell-extra-args is the command arguments which will *always* be used when start ispell process
+  ;; ;; Please note when you use hunspell, ispell-extra-args will NOT be used.
+  ;; ;; Hack ispell-local-dictionary-alist instead.
+  ;; (setq-default ispell-extra-args (flyspell-detect-ispell-args t))
+  ;; ;; (setq ispell-cmd-args (flyspell-detect-ispell-args))
+  ;; (defadvice ispell-word (around my-ispell-word activate)
+  ;;   (let ((old-ispell-extra-args ispell-extra-args))
+  ;;     (ispell-kill-ispell t)
+  ;;     (setq ispell-extra-args (flyspell-detect-ispell-args))
+  ;;     ad-do-it
+  ;;     (setq ispell-extra-args old-ispell-extra-args)
+  ;;     (ispell-kill-ispell t)
+  ;;     ))
+
+  (defadvice flyspell-auto-correct-word (around my-flyspell-auto-correct-word activate)
+    (let ((old-ispell-extra-args ispell-extra-args))
+      (ispell-kill-ispell t)
+      ;; use emacs original arguments
+      (setq ispell-extra-args (flyspell-detect-ispell-args))
+      ad-do-it
+      ;; restore our own ispell arguments
+      (setq ispell-extra-args old-ispell-extra-args)
+      (ispell-kill-ispell t)
+      ))
+
+  (defun text-mode-hook-setup ()
+    ;; Turn off RUN-TOGETHER option when spell check text-mode
+    (setq-local ispell-extra-args (flyspell-detect-ispell-args)))
+  (add-hook 'text-mode-hook 'text-mode-hook-setup)
+
   (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
   (autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
   (autoload 'tex-mode-flyspell-verify "flyspell" "" t)
@@ -42,6 +73,4 @@
      text-mode-hook
      prog-mode-hook
      ))
-  :bind
-  ("C-;" . nil)
   )
