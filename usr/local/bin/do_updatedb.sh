2bdbaf83b18c31133151b5d2c4102d034fea4a42
yhiraki <coffexpr@gmail.com>
2018-05-10 12:06:10 +0900

    mod do_updatedb.sh

--- usr/local/bin/do_updatedb.sh
+++ usr/local/bin/do_updatedb.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+set -eu
+
+log_file=/tmp/updatedb.log
 mnt_list=($HOME/mnt/*)
 
-if echo $mnt_list | tr ' ' '\n' | head -1 | ls > /dev/null 2&>1
+if echo $mnt_list | tr ' ' '\n' | head -1 | xargs ls > /dev/null 2>&1
 then
-  updatedb && date +%s > $last_update_path > /dev/null
+  echo "start updatedb : $(date)" > $log_file
+  /usr/libexec/locate.updatedb >> $log_file 2>&1
+  echo "end   updatedb : $(date)" >> $log_file
+else
+  echo updatedb canceled
 fi
