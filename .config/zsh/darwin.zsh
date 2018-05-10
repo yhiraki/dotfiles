879ca6361223f6475311bf5ad5923b6d4c8f2962
yhiraki <coffexpr@gmail.com>
2018-05-10 12:06:42 +0900

    fix bug

--- .config/zsh/darwin.zsh
+++ .config/zsh/darwin.zsh
@@ -3,7 +3,6 @@ alias ll="ls -lG"
 alias la="ls -laG"
 alias sed="gsed"
 alias updatedb='nice -n 10 /usr/libexec/locate.updatedb'
-alias locate="locate -d $FCODES"
 
 # updatedb settings
 mnt_list=($HOME/mnt/*)
@@ -12,5 +11,6 @@ export LOCATE_PATH=$FCODES
 export FILESYSTEMS="hfs ufs smbfs"
 export PRUNEPATHS="/private/tmp /private/var/folders /private/var/tmp */Backups.backupdb */.git"
 export SEARCHPATHS="/ $mnt_list"
-
 mkdir -p $(dirname $FCODES)
+
+alias locate="locate -d $FCODES"
