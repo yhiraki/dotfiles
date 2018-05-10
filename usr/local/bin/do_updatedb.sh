24159c20820fd78d017f2dd1f0a4ef9cea4744ba
yhiraki <coffexpr@gmail.com>
2018-05-10 13:15:31 +0900

    mod do_updatedb.sh

--- usr/local/bin/do_updatedb.sh
+++ usr/local/bin/do_updatedb.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-set -eu
-
+updatedb='nice -n 10 /usr/libexec/locate.updatedb'
 log_file=/tmp/updatedb.log
-mnt_list=($HOME/mnt/*)
+mnt_list=$(ls $HOME/mnt)
 
-if echo $mnt_list | tr ' ' '\n' | head -1 | xargs ls > /dev/null 2>&1
-then
-  echo "start updatedb : $(date)" > $log_file
-  /usr/libexec/locate.updatedb >> $log_file 2>&1
-  echo "end   updatedb : $(date)" >> $log_file
-else
-  echo updatedb canceled
-fi
+FCODES="$HOME/var/db/locate.database"
+LOCATE_PATH=$FCODES
+FILESYSTEMS="hfs ufs smbfs"
+PRUNEPATHS="/Applications /System /private/tmp /private/var/folders /private/var/tmp */Backups.backupdb */.git */*.lnk"
+SEARCHPATHS="/ $mnt_list"
+mkdir -p $(dirname $FCODES)
+
+for d in $mnt_list
+do
+  if ! ls $d > /dev/null 2>&1
+  then
+    echo could not access to dir: $d
+    echo updatedb canceled
+    exit 1
+  fi
+done
+
+echo start updatedb
+echo "start updatedb : $(date)" >> $log_file
+$updatedb >> $log_file 2>&1
+echo "end   updatedb : $(date)" >> $log_file
