08d19a2e09f84f447f25ce99b81395c012f4c302
yhiraki <coffexpr@gmail.com>
2018-05-11 11:16:57 +0900

    update do_updatedb.sh

--- usr/local/bin/do_updatedb.sh
+++ usr/local/bin/do_updatedb.sh
@@ -2,11 +2,10 @@
 
 updatedb='nice -n 10 /usr/libexec/locate.updatedb'
 log_file=/tmp/updatedb.log
-mnt_list=$(find $HOME/mnt -maxdepth 1 -type d)
 
 mkdir -p $(dirname $FCODES)
 
-for d in $mnt_list
+for d in $SEARCHPATHS
 do
   if ! find $d -maxdepth 1 > /dev/null 2>&1
   then
