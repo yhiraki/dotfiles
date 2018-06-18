9e545aa7fb70d73e9f256967122195d62bab00f6
yhiraki <coffexpr@gmail.com>
2018-06-18 15:17:18 +0900

    reinstall exec path from shell

--- emacs/init/path.el
+++ emacs/init/path.el
@@ -1,10 +1,11 @@
-;; (use-package exec-path-from-shell
-;;   :ensure t
-;;   :defer t
-;;   :init
-;;   (add-hook 'emacs-startup-hook
-;;             '(lambda()
-;;                (when (memq window-system '(mac ns x))
-;;                  (exec-path-from-shell-initialize)))
-;;             )
-;;   )
+;; required for counsel-ghq, etc.
+(use-package exec-path-from-shell
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'emacs-startup-hook
+            '(lambda()
+               (when (memq window-system '(mac ns x))
+                 (exec-path-from-shell-initialize)))
+            )
+  )
