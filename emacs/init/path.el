9dee154ed0ab388869a4215395e12231825f8c1a
yhiraki <coffexpr@gmail.com>
2018-06-20 11:13:08 +0900

    re-add exec-path-from-shell

--- emacs/init/path.el
+++ emacs/init/path.el
@@ -1,11 +1,7 @@
 ;; required for counsel-ghq, etc.
 (use-package exec-path-from-shell
   :ensure t
-  :defer t
   :init
-  (add-hook 'emacs-startup-hook
-            '(lambda()
-               (when (memq window-system '(mac ns x))
-                 (exec-path-from-shell-initialize)))
-            )
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
   )
