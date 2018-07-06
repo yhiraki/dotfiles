1ad3a7cc4ce56a2d31693570d9b260fbd0916655
coffexpr <coffexpr@gmail.com>
2018-07-06 23:12:13 +0900

    add emacs-daemon settings

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -3,7 +3,12 @@ alias ma='my-manual'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
+function emacsclient-gui {
+  local filename=$1
+  emacsclient -c $filename &
+}
 alias e=$EDITOR
+alias ge=emacsclient-gui
 
 alias -g F="| fzf "
 alias -g FP="| fzf --preview '$FZF_PREVIEW_CMD'"
