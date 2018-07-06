99c074b2c2dbb05685ea09e5b15d131deed6e6d6
yhiraki <coffexpr@gmail.com>
2018-07-06 23:06:16 +0900

    update emacs settings

--- emacs/init/path.el
+++ emacs/init/path.el
@@ -1,7 +1,10 @@
-;; required for counsel-ghq, etc.
-(use-package exec-path-from-shell
-  :ensure t
-  :init
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize))
-  )
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c '_anyenv_init; echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
