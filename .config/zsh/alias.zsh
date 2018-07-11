a16f023429039fc26991d4bfec1ce22b4b41ce82
coffexpr <coffexpr@gmail.com>
2018-07-11 18:18:19 +0900

    add locate alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -10,6 +10,12 @@ function emacsclient-gui {
 alias e=$EDITOR
 alias ge=emacsclient-gui
 
+function locate-and-open {
+  local query=$1
+  locate -i $query | fzf | xargs open
+}
+alias f=locate-and-open
+
 alias -g F="| fzf "
 alias -g FP="| fzf --preview '$FZF_PREVIEW_CMD'"
 alias -g O='F | xargs open'
