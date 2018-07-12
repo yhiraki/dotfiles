7e69b2cda08d98ef69d013b3c29ba166efe2e5e1
coffexpr <coffexpr@gmail.com>
2018-07-12 20:07:12 +0900

    virtualenv settings

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -1,17 +1,25 @@
 (use-package switch-buffer-functions
   :ensure t
   :init
-  (defvar pyenv-dir "~/.anyenv/envs/pyenv/")
-  (setq my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text (concat pyenv-dir "/version") 'utf-8)))
-        jedi:server-args (list "--virtual-env" my/current-virtual-env))
+  ;; (defvar pyenv-dir "~/.anyenv/envs/pyenv/")
+  ;; (setq my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text (concat pyenv-dir "/version") 'utf-8)))
+  ;;       jedi:server-args (list "--virtual-env" my/current-virtual-env))
+  ;; (defun switch-jedi-server ()
+  ;;   "Automatically activates pyenv version if .python-version file exists."
+  ;;   (f-traverse-upwards
+  ;;    (lambda (path)
+  ;;      (let ((pyenv-version-path (f-expand ".python-version" path)))
+  ;;        (cond ((f-exists? pyenv-version-path)
+  ;;               (setq-default my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text pyenv-version-path 'utf-8)))
+  ;;                             jedi:server-args (list "--virtual-env" my/current-virtual-env))
+  ;;               ))))))
   (defun switch-jedi-server ()
     "Automatically activates pyenv version if .python-version file exists."
     (f-traverse-upwards
      (lambda (path)
-       (let ((pyenv-version-path (f-expand ".python-version" path)))
-         (cond ((f-exists? pyenv-version-path)
-                (setq-default my/current-virtual-env (concat pyenv-dir "versions/" (s-trim (f-read-text pyenv-version-path 'utf-8)))
-                              jedi:server-args (list "--virtual-env" my/current-virtual-env))
+       (let ((venv-path (f-expand ".venv" path)))
+         (cond ((f-exists? venv-path)
+                (setq jedi:server-args (list "--virtual-env" venv-path))
                 ))))))
   (add-hook 'switch-buffer-functions
             (lambda (prev cur) (switch-jedi-server)))
