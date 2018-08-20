827a5d1c3945e149a0a8e0ec75b6f39a77e5405d
yhiraki <coffexpr@gmail.com>
2018-08-20 15:56:39 +0900

    add prittier for js and ts

--- emacs/init/javascript.el
+++ emacs/init/javascript.el
@@ -7,7 +7,13 @@
 
 (use-package js
   :init
+  (setq js-indent-level 2)
   :defer t
   :config
   (add-to-list 'company-backends 'company-tern)
+  )
+
+(use-package add-node-modules-path
+  :ensure t
+  :hook (js-mode typescript-mode)
   )
