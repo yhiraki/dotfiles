c4b55a027ae95da3cb0cd4ed2c6efed57b736d94
yhiraki <coffexpr@gmail.com>
2018-09-07 20:42:18 +0900

    markdown settings

--- emacs/init/markdown.el
+++ emacs/init/markdown.el
@@ -3,15 +3,11 @@
   :defer t
   :init
   (setq markdown-command "pandoc -s --self-contained -t html5 -c ~/.emacs.d/css/github.css")
-  ;; (add-hook 'markdown-mode-hook
-  ;;           '(lambda ()
-  ;;              (hide-sublevels 3)))
+  (add-hook 'markdown-mode-hook
+            '(lambda ()
+               (hide-sublevels 3)))
   :commands
   (markdown-mode gfm-mode)
-  :bind
-  ;; (:map markdown-mode-map
-  ;;       ("<tab>" . 'markdown-cycle)
-  ;;       ("<S-tab>" . 'markdown-shifttab))
   :mode
   ("\\.markdown\\'" . markdown-mode)
   ("\\.md\\'" . markdown-mode)
