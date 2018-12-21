c193fe8727f98c583e942f4287274a1cfcb27a73
yhiraki <coffexpr@gmail.com>
2018-12-21 15:18:33 +0900

    add custom prompt draft

---
+++ .config/zsh/prompt.zsh
@@ -0,0 +1,30 @@
+# VCSの情報を取得するzsh関数
+autoload -Uz vcs_info
+autoload -Uz colors # black red green yellow blue magenta cyan white
+colors
+
+# PROMPT変数内で変数参照
+setopt prompt_subst
+
+zstyle ':vcs_info:git:*' check-for-changes true #formats 設定項目で %c,%u が使用可
+zstyle ':vcs_info:git:*' stagedstr "%F{green}!" #commit されていないファイルがある
+zstyle ':vcs_info:git:*' unstagedstr "%F{magenta}+" #add されていないファイルがある
+zstyle ':vcs_info:*' formats "%F{15}%c%u(%b)%f" #通常
+zstyle ':vcs_info:*' actionformats '[%b|%a]' #rebase 途中,merge コンフリクト等 formats 外の表示
+
+# %b ブランチ情報
+# %a アクション名(mergeなど)
+# %c changes
+# %u uncommit
+
+# プロンプト表示直前に vcs_info 呼び出し
+precmd () { vcs_info }
+
+# プロンプト（左）
+PROMPT='%{${fg[blue]}%}%~%{${reset_color}%} ${vcs_info_msg_0_}
+❯ '
+# PROMPT=$PROMPT'${vcs_info_msg_0_} %{${fg[red]}%}%}$%{${reset_color}%} '
+# RPPROMT=$PROMPT
+
+# プロンプト（右）
+# RPROMPT='%{${fg[red]}%}[%~]%{${reset_color}%}'
