bf0cb9c7d884857104a270f013a0da42505d0e84
yhiraki <coffexpr@gmail.com>
2019-01-24 16:44:09 +0900

    add settings for sql-indent

--- emacs/init.el
+++ emacs/init.el
@@ -890,47 +890,48 @@ See `org-capture-templates' for more information."
   ("\\.zsh\\'" . shell-script-mode)
   )
 
-;; (use-package sql-indent
-;;   :init
-;;   (el-get-bundle alex-hhh/emacs-sql-indent)
-;;   (defvar my-sql-indentation-offsets-alist
-;;     `((select-clause 0)
-;;       (insert-clause 0)
-;;       (delete-clause 0)
-;;       (update-clause 0)
-;;       (select-join-condition 0)
-;;       (in-select-clause + sqlind-lineup-close-paren-to-open-indentation)
-;;       (select-table-continuation + sqlind-lineup-close-paren-to-open-indentation)
-;;       ,@sqlind-default-indentation-offsets-alist))
-;;   (add-hook 'sqlind-minor-mode-hook
-;;             '(lambda ()
-;;               (setq sqlind-indentation-offsets-alist
-;;                     my-sql-indentation-offsets-alist)))
-;;   (add-hook 'sql-mode-hook
-;;             '(lambda ()
-;;               (sqlind-minor-mode)
-;;               (setq-default sql-indent-offset 2
-;;                             indent-tabs-mode nil
-;;                             c-basic-offset 2
-;;                             tab-width 2)
-;;               (setq sql-mysql-login-params (append sql-mysql-login-params '(port)))
-;;               (sql-set-product "postgres")
-;;               ))
-;;   (add-hook 'sql-interactive-mode-hook
-;;             '(lambda ()
-;;               (toggle-truncate-lines t)))
+(use-package sql-indent :ensure t
+  :config
+  (defvar my-sql-indentation-offsets-alist
+    `((select-clause)
+      (insert-clause 0)
+      (delete-clause 0)
+      (update-clause 0)
+      (select-column + sqlind-adjust-comma)
+      (select-join-condition +)
+      (in-select-clause + sqlind-lineup-close-paren-to-open-indentation)
+      (select-table-continuation + sqlind-lineup-close-paren-to-open-indentation)
+      ,@sqlind-default-indentation-offsets-alist))
+  :init
+  (add-hook 'sqlind-minor-mode-hook
+            '(lambda ()
+              (setq sqlind-indentation-offsets-alist
+                    my-sql-indentation-offsets-alist)))
+  (add-hook 'sql-mode-hook
+            '(lambda ()
+              (sqlind-minor-mode)
+              (setq-default sql-indent-offset 2
+                            indent-tabs-mode nil
+                            c-basic-offset 2
+                            tab-width 2)
+              (setq sql-mysql-login-params (append sql-mysql-login-params '(port)))
+              (sql-set-product "postgres")
+              ))
+  (add-hook 'sql-interactive-mode-hook
+            '(lambda ()
+              (toggle-truncate-lines t)))
 
-;;   ;; https://github.com/xlighting/happy-emacs.d/blob/12e8369cd7934600703b61bb1c278d77dab0c3a2/modules/init-sql.el
-;;   (defun sql-add-newline-first (output)
-;;     "In a SQLi buffer,The table formatting is ugly because the top boundary of the
-;;     table is printed on the same row as the the prompt,This fixes it"
-;;     (replace-regexp-in-string "\\(\\w+[ ]?\\[\\((?[[:alpha:]])?\\|_\\)+\\][#>][ ]?\\)\\(.*[#>] \\)?" "\\1\n" output))
-;;   (defun sqli-add-hooks ()
-;;     "Add hooks to `sql-interactive-mode-hook'."
-;;     (add-hook 'comint-preoutput-filter-functions
-;;               'sql-add-newline-first))
-;;   (add-hook 'sql-interactive-mode-hook 'sqli-add-hooks)
-;;   )
+  ;; https://github.com/xlighting/happy-emacs.d/blob/12e8369cd7934600703b61bb1c278d77dab0c3a2/modules/init-sql.el
+  (defun sql-add-newline-first (output)
+    "In a SQLi buffer,The table formatting is ugly because the top boundary of the
+    table is printed on the same row as the the prompt,This fixes it"
+    (replace-regexp-in-string "\\(\\w+[ ]?\\[\\((?[[:alpha:]])?\\|_\\)+\\][#>][ ]?\\)\\(.*[#>] \\)?" "\\1\n" output))
+  (defun sqli-add-hooks ()
+    "Add hooks to `sql-interactive-mode-hook'."
+    (add-hook 'comint-preoutput-filter-functions
+              'sql-add-newline-first))
+  (add-hook 'sql-interactive-mode-hook 'sqli-add-hooks)
+  )
 
 (use-package toml-mode :ensure t :defer t)
 
