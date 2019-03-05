5484a77a2ceb39f62b7e8a9b66796e59b3942368
yhiraki <coffexpr@gmail.com>
2019-03-05 12:29:32 +0900

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -136,6 +136,14 @@ When ARG is non-nil search in junk files."
                ))
   )
 
+(use-package highlight-indent-guides :ensure t
+  :hook
+  ((prog-mode yaml-mode) . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-method 'character)
+  (highlight-indent-guides-responsive t)
+  )
+
 (use-package eldoc
   :commands eldoc-mode
   :init
@@ -241,9 +249,20 @@ When ARG is non-nil search in junk files."
   (setq wdired-allow-to-change-permissions t)
   )
 
+(use-package flymake
+  :config
+  (set-face-underline 'flymake-error nil)
+  (set-face-underline 'flymake-note nil)
+  (set-face-underline 'flymake-warning nil)
+  :custom
+  (flymake-error-bitmap nil)
+  (flymake-note-bitmap nil)
+  (flymake-warning-bitmap nil)
+  )
+
 (use-package flycheck :ensure t
   :commands flycheck-mode
-  :hook ((js2-mode web-mode plantuml-mode) . flycheck-mode)
+  :hook ((js2-mode python-mode web-mode plantuml-mode) . flycheck-mode)
   :config
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   (flycheck-add-mode 'javascript-eslint 'css-mode)
@@ -515,114 +534,6 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 ;; (use-package company-flx :disabled t)
 
 ;; (use-package company-racer :disabled t)
-
-(use-package modeline :no-require
-  :config
-  (setq-default
-   mode-line-format
-   '(;; Position, including warning for 80 columns
-     (:propertize "%4l:" face mode-line-position-face)
-     (:eval (propertize "%3c" 'face
-                        (if (>= (current-column) 80)
-                            'mode-line-80col-face
-                          'mode-line-position-face)))
-     ;; emacsclient [default -- keep?]
-     mode-line-client
-     ;; read-only or modified status
-     (:eval
-      (concat "  "
-              (cond (buffer-read-only
-                     (propertize " RO " 'face 'mode-line-read-only-face))
-                    ((buffer-modified-p)
-                     (propertize " ** " 'face 'mode-line-modified-face)))))
-     "  "
-     ;; directory and buffer/file name
-     (:propertize (:eval (shorten-directory default-directory 15))
-                  face mode-line-folder-face)
-     (:propertize "%b"
-                  face mode-line-filename-face)
-     ;; narrow [default -- keep?]
-     " %n "
-     ;; mode indicators: vc, recursive edit, major mode, minor modes, process, global
-     (vc-mode vc-mode)
-     "  %["
-     (:propertize mode-name
-                  face mode-line-mode-face)
-     "%] "
-     (:eval (propertize (format-mode-line minor-mode-alist)
-                        'face 'mode-line-minor-mode-face))
-     (:propertize mode-line-process
-                  face mode-line-process-face)
-     (global-mode-string global-mode-string)
-     ))
-
-  ;; Helper function
-  (defun shorten-directory (dir max-length)
-    "Show up to `max-length' characters of a directory name `dir'."
-    (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-          (output ""))
-      (when (and path (equal "" (car path)))
-        (setq path (cdr path)))
-      (while (and path (< (length output) (- max-length 4)))
-        (setq output (concat (car path) "/" output))
-        (setq path (cdr path)))
-      (when path
-        (setq output (concat ".../" output)))
-      output))
-
-  ;; Extra mode line faces
-  (make-face 'mode-line-read-only-face)
-  (make-face 'mode-line-modified-face)
-  (make-face 'mode-line-folder-face)
-  (make-face 'mode-line-filename-face)
-  (make-face 'mode-line-position-face)
-  (make-face 'mode-line-mode-face)
-  (make-face 'mode-line-minor-mode-face)
-  (make-face 'mode-line-process-face)
-  (make-face 'mode-line-80col-face)
-
-  (set-face-attribute 'mode-line nil
-                      :foreground "gray60" :background "gray20"
-                      :inverse-video nil
-                      :box '(:line-width 6 :color "gray20" :style nil))
-  (set-face-attribute 'mode-line-inactive nil
-                      :foreground "gray80" :background "gray40"
-                      :inverse-video nil
-                      :box '(:line-width 6 :color "gray40" :style nil))
-
-  (set-face-attribute 'mode-line-read-only-face nil
-                      :inherit 'mode-line-face
-                      :foreground "#4271ae"
-                      :box '(:line-width 2 :color "#4271ae"))
-  (set-face-attribute 'mode-line-modified-face nil
-                      :inherit 'mode-line-face
-                      :foreground "#c82829"
-                      :background "#ffffff"
-                      :box '(:line-width 2 :color "#c82829"))
-  (set-face-attribute 'mode-line-folder-face nil
-                      :inherit 'mode-line-face
-                      :foreground "gray60")
-  (set-face-attribute 'mode-line-filename-face nil
-                      :inherit 'mode-line-face
-                      :foreground "#eab700"
-                      :weight 'bold)
-  (set-face-attribute 'mode-line-position-face nil
-                      :inherit 'mode-line-face
-                      :family "Menlo" :height 100)
-  (set-face-attribute 'mode-line-mode-face nil
-                      :inherit 'mode-line-face
-                      :foreground "gray80")
-  (set-face-attribute 'mode-line-minor-mode-face nil
-                      :inherit 'mode-line-mode-face
-                      :foreground "gray40"
-                      :height 110)
-  (set-face-attribute 'mode-line-process-face nil
-                      :inherit 'mode-line-face
-                      :foreground "#718c00")
-  (set-face-attribute 'mode-line-80col-face nil
-                      :inherit 'mode-line-position-face
-                      :foreground "black" :background "#eab700")
-  )
 
 (use-package quickrun :ensure t
   :commands quickrun
