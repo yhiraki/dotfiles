ef048a34914519836111e2a39f1892d3acb899a1
yhiraki <coffexpr@gmail.com>
2019-04-12 18:52:26 +0900

    update mac settings

--- mac.sh
+++ mac.sh
@@ -1,2 +1,20 @@
+#!/bin/bsah
+
 # Finderで隠しファイルを表示する
 defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# GUI apps
+brew cask install \
+     chrome \
+     firefox \
+     slack \
+     alacritty \
+
+# TODO :
+# Microsoft Remote desktop
+# Amphetamine
+# Irvine (wallpaper)
+# docker
+# line
+# dropbox
+# display menu
