4a88e512b78db64f1f0a096bff956d940c07ea79
yhiraki <coffexpr@gmail.com>
2019-05-13 18:38:26 +0900

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -201,20 +201,20 @@ When ARG is non-nil search in junk files."
     (find-file-other-window user-init-file))
   )
 
-(use-package ime :no-require
-  :init
-  (add-hook 'evil-normal-state-entry-hook
-            '(lambda ()
-               (mac-toggle-input-method nil)))
-  (add-hook 'evil-normal-state-entry-hook 'mac-change-language-to-us)
-  ;; ミニバッファを開いたときに英字にする（閉じてもモードは戻らない）
-  (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
-  :config
-  ;; http://blog.ichiroc.in/entry/2013/09/06/075832
-  ;; Google日本語入力をベースにする
-  ;; これがないと(mac-toggle-input-method t) で、ことえりが有効になってしまう。
-  (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
-  )
+;; (use-package ime :no-require
+;;   :init
+;;   (add-hook 'evil-normal-state-entry-hook
+;;             '(lambda ()
+;;                (mac-toggle-input-method nil)))
+;;   (add-hook 'evil-normal-state-entry-hook 'mac-change-language-to-us)
+;;   ;; ミニバッファを開いたときに英字にする（閉じてもモードは戻らない）
+;;   (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
+;;   :config
+;;   ;; http://blog.ichiroc.in/entry/2013/09/06/075832
+;;   ;; Google日本語入力をベースにする
+;;   ;; これがないと(mac-toggle-input-method t) で、ことえりが有効になってしまう。
+;;   (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
+;;   )
 
 (use-package appearance :no-require
   :config
