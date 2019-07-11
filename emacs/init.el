966e8d3a757abc3de562fee7e61975bdc322fc70
yhiraki <coffexpr@gmail.com>
2019-07-11 11:20:59 +0900

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -503,19 +503,19 @@ When ARG is non-nil search in junk files."
   :commands (counsel-ghq)
   )
 
-;; (use-package path :no-require
-;;   :config
-;;   (defun set-exec-path-from-shell-PATH ()
-;;     "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+(use-package path :no-require
+  :config
+  (defun set-exec-path-from-shell-PATH ()
+    "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
 
-;; This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-;;     (interactive)
-;;     (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-;;       (setenv "PATH" path-from-shell)
-;;       (setq exec-path (split-string path-from-shell path-separator))))
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+    (interactive)
+    (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+      (setenv "PATH" path-from-shell)
+      (setq exec-path (split-string path-from-shell path-separator))))
 
-;;   (set-exec-path-from-shell-PATH)
-;;   )
+  (set-exec-path-from-shell-PATH)
+  )
 
 ;; (use-package pangu-spacing :ensure t
 ;;   :init
@@ -658,6 +658,10 @@ When ARG is non-nil search in junk files."
     '((:command . "python3")
       (:compile-only . "flake8 %s")))
   (quickrun-set-default "python" "python3")
+
+  (quickrun-add-command "typescript"
+    '((:exec . ("%c --target es6 --module commonjs %o %s %a" "node %n.js")))
+    :override t)
   )
 
 (use-package csharp-mode :ensure t
