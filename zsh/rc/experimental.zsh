4578b9f34d9dad9b26a9adedeb8638459947e6e7
yhiraki <coffexpr@gmail.com>
2019-09-11 10:57:10 +0900

    delete zplug settings

--- zsh/rc/experimental.zsh
+++
@@ -1,9 +0,0 @@
-# https://dev.classmethod.jp/tool/fzf-original-app-for-git-add/
-function gadd() {
-    local selected
-    selected=$(git status -s | fzf -m | awk '{print $2}')
-    if [[ -n "$selected" ]]
-    then
-        echo $selected | xargs git add
-    fi
-}
