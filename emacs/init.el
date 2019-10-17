9fbf7aad6292e707e3e4a6a872997dcd71be6be4
yhiraki <coffexpr@gmail.com>
2019-10-17 20:23:28 +0900

    emacs: clipboard

--- emacs/init.el
+++ emacs/init.el
@@ -32,6 +32,34 @@
 
 (straight-use-package 'use-package)
 
+(use-package global :no-require
+  :config
+  ;; system-type predicates
+  ;; from http://d.hatena.ne.jp/tomoya/20090807/1249601308
+  (setq darwin-p   (eq system-type 'darwin)
+        linux-p    (eq system-type 'gnu/linux)
+        carbon-p   (eq system-type 'mac)
+        meadow-p   (featurep 'meadow))
+  )
+
+(use-package clipboard :no-require
+  :config
+  ;; Emacs と Mac のクリップボード共有
+  ;; from http://hakurei-shain.blogspot.com/2010/05/mac.html
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (if (or darwin-p carbon-p)
+      (setq interprogram-cut-function 'paste-to-osx)
+    (setq interprogram-paste-function 'copy-from-osx))
+  )
+
 (use-package startup :no-require
   :custom
   (confirm-kill-emacs 'y-or-n-p)
