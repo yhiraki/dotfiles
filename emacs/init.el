26738dda8d04b68d64b331b93a5fbad061bb7ffe
yhiraki <coffexpr@gmail.com>
2019-10-17 22:46:45 +0900

    emacs: delete clipboard shareing settings

--- emacs/init.el
+++ emacs/init.el
@@ -42,24 +42,6 @@
         meadow-p   (featurep 'meadow))
   )
 
-(use-package clipboard :no-require
-  :config
-  ;; Emacs と Mac のクリップボード共有
-  ;; from http://hakurei-shain.blogspot.com/2010/05/mac.html
-  (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
-
-  (defun paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-        (process-send-string proc text)
-        (process-send-eof proc))))
-
-  (if (or darwin-p carbon-p)
-      (setq interprogram-cut-function 'paste-to-osx)
-    (setq interprogram-paste-function 'copy-from-osx))
-  )
-
 (use-package startup :no-require
   :custom
   (confirm-kill-emacs 'y-or-n-p)
