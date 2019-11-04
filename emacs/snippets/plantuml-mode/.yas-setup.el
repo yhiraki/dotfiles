3f836b63d3dff62f9e9be450a3d511e9d819780f
yhiraki <coffexpr@gmail.com>
2019-11-04 15:47:54 +0900

    emacs: snippets add plantuml arrow

--- emacs/snippets/plantuml-mode/.yas-setup.el
+++ emacs/snippets/plantuml-mode/.yas-setup.el
@@ -1,9 +1,21 @@
 (require 'yasnippet)
 
-(defvar plantuml-arrow-alist
+(defvar plantuml-arrow-right-alist
       '(
-        "-> : hoge"
-        ".> : ふが"
+        "->  ｜ Arrow"
+        "-|> ｜ [Class] Extension"
+        "-*  ｜ [Class] Composition"
+        "-o  ｜ [Class] Aggretation"
+        "-#  ｜ [Class]"
+        "-x  ｜ [Class]"
+        "-}  ｜ [Class]"
+        "-+  ｜ [Class]"
+        "-^  ｜ [Class]"
         ))
 
-(defvar plantuml-arrow-cut-description-point 2)
+(defvar plantuml-description-delimiter "｜")
+
+(defun plantuml-choose (alist)
+  (string-trim-right
+   (nth 0 (split-string
+           (yas-choose-value alist) plantuml-description-delimiter))))
