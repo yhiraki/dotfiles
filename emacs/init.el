462d44f50d5d0c3c2c7911caf685a747c33b130a
yhiraki <coffexpr@gmail.com>
2020-02-19 15:51:49 +0900

    emacs: update snippets, templates

--- emacs/init.el
+++ emacs/init.el
@@ -2270,6 +2270,7 @@ _p_revious  ^ ^ | _d_elete      | ^ ^             |
                   (("\\.py\\'" "Python script") . ["template.py" my/autoinsert-yas-expand])
                   (("\\.vue\\'" "Vue") . ["template.vue" my/autoinsert-yas-expand])
                   ((plantuml-mode "Plantuml") . ["template.plantuml" my/autoinsert-yas-expand])
+                  ((sh-mode "Shell script") . ["template.sh" my/autoinsert-yas-expand])
                   )
                 auto-insert-alist)
         )
