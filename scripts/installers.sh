38086170dee8693b760edc2907c2843385f6760e
yhiraki <coffexpr@gmail.com>
2020-02-26 14:09:23 +0900

    version

--- scripts/installers.sh
+++ scripts/installers.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
+
 install_emacs() {
+  local version
+  version=${1:=26.3}
   case "${OSNAME}" in
-    osx) brew install emacs ;;
-    linux) sudo apt-get install emacs ;;
+  osx)
+    brew cask install emacs@${version}
+    ;;
+  linux)
+    sudo add-apt-repository ppa:kelleyk/emacs
+    case "${version}" in
+      25*) apt-get install -y emacs25=${version}* ;;
+      26*) apt-get install -y emacs26=${version}* ;;
+      *) exit 1
+    esac
+    ;;
   esac
 }
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
 setup_env() {
+  DEBIAN_FRONTEND=noninteractive
+
   _ostype() {
     case $(uname | tr '[:upper:]' '[:lower:]') in
       linux*) echo linux ;;
@@ -11,16 +13,22 @@ setup_env() {
   unset -f _ostype
 
   export OSNAME
+  export DEBIAN_FRONTEND
 }
 
 setup_package_manager() {
   case "${OSNAME}" in
-    osx) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ;;
-    linux) sudo apt-get update ;;
+    osx)
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      ;;
+    linux)
+      sudo apt-get update
+      sudo apt-get install -y software-properties-common
+      ;;
   esac
 }
 
-setup () {
+setup() {
   setup_env
   setup_package_manager
 }
