3589ca9c3dd57b46fb5dad131e143be3767306ec
yhiraki <coffexpr@gmail.com>
2020-02-26 14:32:55 +0900

    shfmt

--- scripts/installers.sh
+++ scripts/installers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-install_essentials () {
+install_essentials() {
   case "${OSNAME}" in
     osx) ;;
     linux) sudo apt-get install -y git ;;
@@ -11,17 +11,17 @@ install_emacs() {
   local version="$1"
   version="${version:=26.3}"
   case "${OSNAME}" in
-  osx)
-    # TODO: switch versions
-    brew cask install emacs
-    ;;
-  linux)
-    sudo add-apt-repository ppa:kelleyk/emacs
-    case "${version}" in
-      25*) sudo apt-get install -y emacs25=${version}* ;;
-      26*) sudo apt-get install -y emacs26=${version}* ;;
-      *) exit 1
-    esac
-    ;;
+    osx)
+      # TODO: switch versions
+      brew cask install emacs
+      ;;
+    linux)
+      sudo add-apt-repository ppa:kelleyk/emacs
+      case "${version}" in
+        25*) sudo apt-get install -y emacs25=${version}* ;;
+        26*) sudo apt-get install -y emacs26=${version}* ;;
+        *) exit 1 ;;
+      esac
+      ;;
   esac
 }
