ab777501be31062ab0805725f4483c4b93facba1
yhiraki <coffexpr@gmail.com>
2020-11-12 23:44:31 +0900

    emacs: quickrun c++14 with cmd options

--- emacs/init.el
+++ emacs/init.el
@@ -1052,11 +1052,15 @@ Version 2019-11-04"
       (:exec    . ("%c script %o %s")))
     :default "rust")
 
-  (quickrun-add-command "c++/g++"
-    '((:exec . ("%c -Wall -Wextra -std=c++14 -fsanitize=undefined %o -o %e %s" "%e %a"))
-      (:compile-only . "%c -Wall -Wextra -std=c++14 -fsanitize=undefined %o -o %e %s"))
-    :override t)
-  (quickrun-set-default "c++" "c++/g++")
+  (quickrun-add-command "c++14/g++"
+    '((:command . "g++")
+      (:exec    . ("%c -x c++ %o -o %e %s" "%e %a"))
+      (:compile-only . "%c %o -o %e %s")
+      (:remove  . ("%e"))
+      (:description . "Compile C++ file with g++ and execute")
+      (:cmdopt . "-Wall -Wextra -pedantic -std=c++14 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlong-long -Wshift-overflow -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector"))
+    )
+  (quickrun-set-default "c++" "c++14/g++")
 
   (quickrun-add-command "typescript"
     '((:exec . ("%c --target es6 --module commonjs %o %s %a" "node %n.js")))
