a1d4e8d0b7aead0cfc84dbfa66bd93d925fc7b72
yhiraki <coffexpr@gmail.com>
2021-05-21 11:28:44 +0900

    emacs: remove trello

--- emacs/init.candidate.el
+++ emacs/init.candidate.el
@@ -1,21 +1,3 @@
-(use-package org-trello :ensure t
-  ;; https://org-trello.github.io/usage.html#automatic-org-trello-files-in-emacs
-  :hook
-  (org-mode
-   . (lambda ()
-       (let ((filename (buffer-file-name (current-buffer))))
-         (when (and filename (string= "trello" (file-name-extension filename)))
-           (org-trello-mode)))))
-  (org-trello-mode
-   . (lambda ()
-       (org-trello-sync-buffer t)
-       (evil-define-key 'normal org-trello-mode-map
-	 (kbd "\\r") 'org-trello-sync-card
-	 (kbd "\\s") 'org-trello-sync-buffer
-	 )))
-
-  :mode
-  ("\\.trello\\'" . org-mode)
   )
 
 (use-package go-translate
