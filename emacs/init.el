60ef15dfb4db5c98b35881f6c1f886836f616d1f
yhiraki <coffexpr@gmail.com>
2021-08-12 20:06:30 +0900

    emacs: update org-capture template

--- emacs/init.el
+++ emacs/init.el
@@ -1604,7 +1604,7 @@ See URL `https://github.com/koalaman/shellcheck/'."
 	  "** MEETING %?\n%T" :jump-to-captured t :clock-in t :clock-keep t)
 	 ("l" "Log - Short logs like Twitter"
 	  entry (file+headline my-org-daily-journal-file "Logs")
-	  "** %T %^{Log}" :immediate-finish t)
+	  "** %T %^{Log} :Log:" :immediate-finish t)
 	 ("B" "Blog - Hugo post"
 	  plain (file+olp "blog.org" "Blog Ideas")
 	  "hugo%?")
