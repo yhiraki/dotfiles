7ee513bbc682babf551ab78ddbe7198be5c34387
yhiraki <coffexpr@gmail.com>
2022-12-08 11:52:42 +0900

    emacs: move fucntion definition

--- emacs/init.el
+++ emacs/init.el
@@ -64,6 +64,45 @@
 (defvar use-package-enable-imenu-support t)  ; Must be set before (require 'use-package)
 (require 'use-package)
 
+(defun my-open-in-external-app (&optional @fname)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+
+When called in emacs lisp, if @fname is given, open that.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-11-04"
+  (interactive)
+  (let* (
+		 ($file-list
+		  (if @fname
+			  (progn (list @fname))
+			(if (string-equal major-mode "dired-mode")
+				(dired-get-marked-files)
+			  (list (buffer-file-name)))))
+		 ($do-it-p (if (<= (length $file-list) 5)
+					   t
+					 (y-or-n-p "Open more than 5 files? "))))
+	(when $do-it-p
+	  (cond
+	   ((string-equal system-type "windows-nt")
+		(mapc
+		 (lambda ($fpath)
+		   (w32-shell-execute "open" $fpath)) $file-list))
+	   ((string-equal system-type "darwin")
+		(mapc
+		 (lambda ($fpath)
+		   (shell-command
+			(concat "open " (shell-quote-argument $fpath))))  $file-list))
+	   ((string-equal system-type "gnu/linux")
+		(mapc
+		 (lambda ($fpath) (let ((process-connection-type nil))
+							(start-process "" nil "xdg-open" $fpath))) $file-list))))))
+
+(defun my-open-current-dir ()
+  (interactive)
+  (shell-command "open ."))
+
 (use-package straight :no-require
   :config
   (defvar bootstrap-version)
@@ -81,52 +120,6 @@
   )
 
 (use-package general :ensure t)
-
-(use-package user-defined-functions :no-require
-  :config
-  (defun my-open-in-external-app (&optional @fname)
-    "Open the current file or dired marked files in external app.
-The app is chosen from your OS's preference.
-
-When called in emacs lisp, if @fname is given, open that.
-
-URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
-Version 2019-11-04"
-    (interactive)
-    (let* (
-           ($file-list
-            (if @fname
-                (progn (list @fname))
-              (if (string-equal major-mode "dired-mode")
-                  (dired-get-marked-files)
-                (list (buffer-file-name)))))
-           ($do-it-p (if (<= (length $file-list) 5)
-                         t
-                       (y-or-n-p "Open more than 5 files? "))))
-      (when $do-it-p
-        (cond
-         ((string-equal system-type "windows-nt")
-          (mapc
-           (lambda ($fpath)
-             (w32-shell-execute "open" $fpath)) $file-list))
-         ((string-equal system-type "darwin")
-          (mapc
-           (lambda ($fpath)
-             (shell-command
-              (concat "open " (shell-quote-argument $fpath))))  $file-list))
-         ((string-equal system-type "gnu/linux")
-          (mapc
-           (lambda ($fpath) (let ((process-connection-type nil))
-                              (start-process "" nil "xdg-open" $fpath))) $file-list))))))
-  )
-
-(use-package user-defined-functions-macos :no-require
-  :if darwin-p
-  :config
-  (defun my-open-current-dir ()
-    (interactive)
-    (shell-command "open ."))
-  )
 
 (use-package emacs
   :hook
