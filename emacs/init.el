51df137e333fd681847c489b0c533cd21c5a3d1a
yhiraki <coffexpr@gmail.com>
2024-06-14 11:49:02 +0900

    emacs: remove org-appear

--- emacs/init.el
+++ emacs/init.el
@@ -2084,24 +2084,6 @@
 
   (use-package org-web-tools :ensure t)
 
-  (use-package org-appear :ensure t
-    :hook
-    (org-mode . org-appear-mode)
-    (org-mode
-     . (lambda ()
-         (add-hook 'evil-insert-state-entry-hook
-                   #'org-appear-manual-start
-                   nil
-                   t)
-         (add-hook 'evil-insert-state-exit-hook
-                   #'org-appear-manual-stop
-                   nil
-                   t)))
-    :custom
-    (org-appear-autolinks t)
-    (org-appear-trigger 'manual)
-    (org-hide-emphasis-markers t)
-    )
   )
 
 (use-package appt
